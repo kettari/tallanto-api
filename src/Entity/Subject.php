f6e58c12ad2b94aafb3f48c5df2eaedb0a127b52
Andy <ant@kaula.ru>
2017-06-18 21:49:26 +0300

    tallanto-api-14 Add getters and setters to the Subject entity

--- src/Entity/Subject.php
+++ src/Entity/Subject.php
@@ -62,4 +62,137 @@ class Subject extends AbstractIdentifiableEntity {
     $this->branches = $this->sanitizeBranch($this->branches);
   }
 
+  /**
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * @param string $name
+   * @return Subject
+   */
+  public function setName(string $name): Subject
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDescription()
+  {
+    return $this->description;
+  }
+
+  /**
+   * @param string $description
+   * @return Subject
+   */
+  public function setDescription(string $description): Subject
+  {
+    $this->description = $description;
+
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getStatus()
+  {
+    return $this->status;
+  }
+
+  /**
+   * @param string $status
+   * @return Subject
+   */
+  public function setStatus(string $status): Subject
+  {
+    $this->status = $status;
+
+    return $this;
+  }
+
+  /**
+   * @return bool
+   */
+  public function isCalendarHidden()
+  {
+    return $this->calendar_hidden;
+  }
+
+  /**
+   * @param bool $calendar_hidden
+   * @return Subject
+   */
+  public function setCalendarHidden(bool $calendar_hidden): Subject
+  {
+    $this->calendar_hidden = $calendar_hidden;
+
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDefaultStakeId()
+  {
+    return $this->default_stake_id;
+  }
+
+  /**
+   * @param string $default_stake_id
+   * @return Subject
+   */
+  public function setDefaultStakeId(string $default_stake_id): Subject
+  {
+    $this->default_stake_id = $default_stake_id;
+
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDateStart()
+  {
+    return $this->date_start;
+  }
+
+  /**
+   * @param string $date_start
+   * @return Subject
+   */
+  public function setDateStart(string $date_start): Subject
+  {
+    $this->date_start = $date_start;
+
+    return $this;
+  }
+
+  /**
+   * @return string
+   */
+  public function getDateFinish()
+  {
+    return $this->date_finish;
+  }
+
+  /**
+   * @param string $date_finish
+   * @return Subject
+   */
+  public function setDateFinish(string $date_finish): Subject
+  {
+    $this->date_finish = $date_finish;
+
+    return $this;
+  }
+
 }
